848394643af76b20d9cf11f896957de05f49d17b
Calebio <Kalebiotezz@yahoo.com>
2022-09-09 11:40:34 +0100

    Change message2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World so this me testing CI/CD'));
+app.get('/', (req, res) => res.send('Hello World so this me testing CI/CD Pipeline yo!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
